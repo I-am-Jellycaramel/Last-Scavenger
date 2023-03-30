a74f50d63ff1f937f3eef95ecb828f09d49a970c
DDang_ <northwind3552@gmail.com>
2023-03-30 20:44:56 +0900

    add(managers): itemManager.py
    추가(managers): itemManager.py 추가

---
+++ src/managers/itemManager.py
@@ -0,0 +1,13 @@
+import main
+
+itemList = []
+
+def getItem(number):
+    for item in itemList:
+        if (item.id != number):
+            continue
+        return item
+        
+
+def setUp():
+    print(main.prefix, "아이템을 초기화합니다.")
