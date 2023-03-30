241000f2c649406c7964dcbeae75a93c042a9819
DDang_ <northwind3552@gmail.com>
2023-03-30 20:42:25 +0900

    add(classes): item.py
    추가(classes): item.py 추가

---
+++ src/classes/item.py
@@ -0,0 +1,14 @@
+import managers.itemManager as itemManager
+import enums.itemType as itemType
+
+class Item():
+
+    #name == 아이템 이름
+    #description == 아이템 설명
+    #type == 아이템 유형
+    def __init__(self, id: int, name: str, description: list, type: itemType.ItemType):
+        self.id = id 
+        self.name = name 
+        self.description = description
+        self.type = type
+        itemManager.itemList.append(self)
