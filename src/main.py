4521b702af08c7dc628285334a60e37208e0a2df
DDang_ <129017246+I-am-Jellycaramel@users.noreply.github.com>
2023-04-10 13:41:25 +0900

    update(main): add main run logic to lastScavenger class
    업데이트(main): lastscavenger 클래스로 메인 실행 로직 옮김

--- src/main.py
+++ src/main.py
@@ -1,87 +1,16 @@
 import os
-import sys
 
 import pygame
 from pygame.locals import *
 
+
 import classes.adventure as adventure
 import classes.house as house
 import classes.config as config
+import classes.lastscavenger as lastScavenger
 import managers.itemManager as itemManager
 import managers.situationManager as situationManager
 import managers.ymlManager as ymlManager
-import managers.renderManager as renderManager
-import enums.gamePhase as gamePhase
-
-
-# 코드 영감: http://pygametutorials.wikidot.com/tutorials-basic
-class LastScavanager():
-
-    # 객체 초기화 될때
-    def __init__(self):
-        self.isRunning = True
-        self.displaySurf = None
-        self.gameFrameWidth = 800
-        self.gameFrameHeight = 600
-        self.colorWhite = (255, 255, 255)
-        self.colorBlack = (0, 0, 0)
-        self.fps = 30
-        self.gameTitle = "Last Scavanger - " + config.version
-        self.font = None
-        self.clock = None
-        self.phase = gamePhase.GamePhase.PHASE_1
-
-    # 게임 초기화 시킬 때
-    def on_init(self):
-        pygame.init()
-
-        # 폰트 설정
-        self.font = pygame.font.SysFont("nanumgothic", 35)
-
-        # 시간 설정
-        self.clock = pygame.time.Clock()
-
-        # 아이콘 설정
-        logo = pygame.image.load("last-scavanger-main-gui-icon.png")
-        pygame.display.set_icon(logo)
-
-        # 창 제목 설정
-        pygame.display.set_caption(self.gameTitle)
-        self.displaySurf = pygame.display.set_mode((self.gameFrameWidth, self.gameFrameHeight))
-
-        self.isRunning = True
-
-    # 이벤트 감지할 때
-    def on_event(self, event):
-        if event.type == pygame.QUIT:
-            self.isRunning = False
-
-    # 반복 작업할 때
-    def on_loop(self):
-        self.clock.tick(self.fps)
-
-    # 그래픽 작업할 때
-    def on_render(self):
-        renderManager.render_screen(self.phase)
-        pygame.display.update()
-
-    # 종료할 때
-    def on_cleanup(self):
-        pygame.quit()
-        sys.exit()
-
-    def on_execute(self):
-        if self.on_init() is False:
-            self.isRunning = False
-
-        while self.isRunning:
-            for event in pygame.event.get():
-                self.on_event(event)
-
-            self.on_loop()
-            self.on_render()
-
-        self.on_cleanup()
 
 
 # 메인 게임 화면에서 선택지 제공
@@ -124,5 +53,5 @@ if __name__ == "__main__":
     configYaml = ymlManager.load_yaml()
     config = config.Config(configYaml)
 
-    instance = LastScavanager()
+    instance = lastScavenger.LastScavenager(config.version)
     instance.on_execute()
