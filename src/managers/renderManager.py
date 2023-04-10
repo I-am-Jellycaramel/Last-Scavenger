da9afd5b833a186ee902edb37371704faa2e4454
DDang_ <129017246+I-am-Jellycaramel@users.noreply.github.com>
2023-04-10 14:13:35 +0900

    update(renderManager): divide render_background
    수정(renderManager): render_background 함수 분할

--- src/managers/renderManager.py
+++ src/managers/renderManager.py
@@ -7,10 +7,14 @@ import classes.lastscavenger as ls
 def render_screen(phase: gamePhase.GamePhase, ls: ls.LastScavenger):
     match phase:
         case phase.PHASE_1:
-            background = pygame.image.load("last-scavanger-game-main-screen.png")
-            game_title = ls.font.render("Last Scavanger", 1, ls.colorWhite)
+            render_background(ls)
 
-            ls.displaySurf.blit(background, (0, 0))
+            #게임 제목 출력
+            game_title = ls.font.render("Last Scavanger", 1, ls.colorWhite)
             ls.displaySurf.blit(game_title, (40, 300))
         case _:
             pass
+
+def render_background(ls: ls.LastScavenger):
+    background = pygame.image.load("last-scavanger-game-main-screen.png")
+    ls.displaySurf.blit(background, (0, 0))
