8d5edc26e20a634756c16862bda79779d8b86dd0
DDang_ <129017246+I-am-Jellycaramel@users.noreply.github.com>
2023-04-11 11:26:39 +0900

    update(renderManager): draw button from situation
    수정(renderManager): situation 에서 버튼 받아와서 화면에 그림

--- src/managers/renderManager.py
+++ src/managers/renderManager.py
@@ -1,10 +1,12 @@
 import pygame
 from pygame.locals import *
 
+import managers.situationManager as situationManager
 import enums.gamePhase as gamePhase
-import classes.lastscavenger as ls
+import classes.lastscavenger as lastscavenger
+import classes.situation as situation
 
-def render_screen(phase: gamePhase.GamePhase, ls: ls.LastScavenger):
+def render_screen(phase: gamePhase.GamePhase, ls: lastscavenger.LastScavenger):
     match phase:
         case phase.PHASE_1:
             render_background(ls)
@@ -12,9 +14,16 @@ def render_screen(phase: gamePhase.GamePhase, ls: ls.LastScavenger):
             #게임 제목 출력
             game_title = ls.font.render("Last Scavanger", 1, ls.colorWhite)
             ls.displaySurf.blit(game_title, (40, 300))
+
+            #상황 가져오기
+            main_screen_situation = situationManager.get_situation(0)
+            if isinstance(main_screen_situation, situation.Situation):
+                buttons = main_screen_situation.buttons
+                for button in buttons:
+                    button.draw(ls.displaySurf, ls.font)
         case _:
             pass
 
-def render_background(ls: ls.LastScavenger):
+def render_background(ls: lastscavenger.LastScavenger):
     background = pygame.image.load("last-scavanger-game-main-screen.png")
     ls.displaySurf.blit(background, (0, 0))
