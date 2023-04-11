b5217b87dd5169ce2e246609401c0058f3ed5f90
DDang_ <129017246+I-am-Jellycaramel@users.noreply.github.com>
2023-04-11 11:24:05 +0900

    update(situation): add buttons
    수정(situation): buttons 필드 추가

--- src/classes/situation.py
+++ src/classes/situation.py
@@ -1,11 +1,16 @@
+from typing import List
+
+
+import classes.button as button
 import enums.area as area
 import managers.situationManager as situationManager
 
-
 class Situation:
-    def __init__(self, id: int, name: str, description: list, area: area.Area):
+    def __init__(self, id: int, name: str, description: List[str], area: area.Area, buttons: List[button.Button]):
         self.id = id
         self.name = name
         self.description = description
         self.area = area
+        self.buttons = buttons
+
         situationManager.situationList.append(self)
