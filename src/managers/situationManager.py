84039e79042d641b96296eca6030e607c0e0879b
DDang_ <129017246+I-am-Jellycaramel@users.noreply.github.com>
2023-04-11 11:28:12 +0900

    update(situationManager): add button logic
    수정(situationManager): 버튼 로직 추가된 situation 에 맞게 인수 넣어줌

--- src/managers/situationManager.py
+++ src/managers/situationManager.py
@@ -1,6 +1,7 @@
 import classes.situation as situation
 import enums.area as area
-import main as main
+import classes.button as button
+import classes.lastscavenger as lastscavenger_import
 
 situationList = []
 
@@ -12,7 +13,7 @@ def get_situation(number):
         return None
 
 
-def setUp():
+def setUp(li: lastscavenger_import.LastScavenger):
     # print(main.prefix, "상황을 초기화합니다.")
     situation.Situation(
         0,
@@ -20,7 +21,10 @@ def setUp():
         [
             "아직 아무 일도 일어나지 않았습니다. 걱정하지 마세요."
         ],
-        area.Area.ROAD_OF_HOME
+        area.Area.ROAD_OF_HOME,
+        [
+            button.Button(li, "아무일도 없는 버튼", (0, 0, 0), (60, 60, 60), (235, 159, 239), 100, 200, 100, 100)
+        ]
     )
     situation.Situation(
         1,
@@ -29,5 +33,6 @@ def setUp():
             "당신은 한낱 잡무용 로봇일 뿐이지만, 가슴이 뜨거워짐을 느꼈습니다.",
             "물론 그렇게 느끼도록 프로그래밍 되어있었겠지만, 오늘도 쓸만한 자원을 모으러 이동해야합니다."
         ],
-        area.Area.ROAD_OF_HOME
+        area.Area.ROAD_OF_HOME,
+        []
     )
